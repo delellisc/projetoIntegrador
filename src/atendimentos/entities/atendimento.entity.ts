39e9807f0170ff2cecdbaaafc6cbfac270c092df
David Araújo <166455153+debiddos@users.noreply.github.com>
2025-01-22 14:22:32 -0300

    modelagem de classe atendimento

--- src/atendimentos/entities/atendimento.entity.ts
+++ src/atendimentos/entities/atendimento.entity.ts
@@ -1,1 +1,21 @@
-export class Atendimento {}
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { Paciente } from '../../pacientes/entities/paciente.entity';
+import { Profissional } from '../../profissionais/entities/profissionai.entity';
+
+@Entity('atendimento')
+export class Atendimento {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'timestamp' })
+  horario: Date;
+
+  @Column()
+  status: string;
+
+  @ManyToOne(() => Paciente, (paciente) => paciente.id)
+  paciente: Paciente;
+
+  @ManyToOne(() => Profissional, (profissional) => profissional.id)
+  profissional: Profissional;
+}
