a8606185fab27053cabeb32e4486db4c41d53923
David Araújo <166455153+debiddos@users.noreply.github.com>
2025-01-22 14:24:29 -0300

    modelagem de classe paciente

--- src/pacientes/entities/paciente.entity.ts
+++ src/pacientes/entities/paciente.entity.ts
@@ -1,1 +1,25 @@
-export class Paciente {}
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+
+@Entity('paciente')
+export class Paciente {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  login: string;
+
+  @Column()
+  senha: string;
+
+  @Column()
+  nome: string;
+
+  @Column()
+  sexo: string;
+
+  @Column({ type: 'date' })
+  data_nascimento: Date;
+
+  @Column()
+  contato: string;
+}
