d5decac27c8d2d7716ee582075c90cd665c7a0b4
David Araújo <166455153+debiddos@users.noreply.github.com>
2025-01-22 14:23:37 -0300

    modelagem de classe profissional

--- src/profissionais/entities/profissionai.entity.ts
+++ src/profissionais/entities/profissionai.entity.ts
@@ -1,1 +1,22 @@
-export class Profissionai {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('profissional')
+export class Profissional {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  login: string;
+
+  @Column()
+  senha: string;
+
+  @Column()
+  nome: string;
+
+  @Column()
+  registro_profissional: string;
+
+  @Column()
+  status: string;
+}
