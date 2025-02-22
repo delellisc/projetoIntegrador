fec3c9684ebcf9302943437b5be9976b538dfa35
arthurvinice <arthurvinice@gmail.com>
2025-02-22 11:23:35 -0300

    corrigindo scprit_paginicial

--- front/public/scripts/script_paginicial.js
+++ front/public/scripts/script_paginicial.js
@@ -1,16 +1,52 @@
-// document.addEventListener("DOMContentLoaded", async () => {
+async function getUserData() {
+    try {
+        const response = await fetch('/auth/user')
+        if (!response.ok) {
+            throw new Error("Falha ao obter dados do usuário");
+        }
+        return await response.json()
+    } catch (erro) {
+        console.log("este foi o erro:", erro)
+        return 
+    }
+}
+
+// async function getAtendimentos(matricula) {
+//     const dados = await getUserData()
+//     const matricula = dados.matricula
 //     try {
-//         const response = await fetch('/auth/user'); //requisicao pra rota que pega o usuario logado
-//         if (!response.ok) {
-//             throw new Error("Falha ao obter dados do usuário");
-//         }
-        
-//         const userData = await response.json();
-//         console.log("Dados do usuário:", userData);
-        
-//         // Exibir os dados do usuário na interface
-//         document.getElementById("user-name").textContent = userData.nome_usual;
-//     } catch (error) {
-//         console.error(error);
+//         return
 //     }
-// });
+// // }
+
+//tentativa de mostrar ultimas consultas
+
+// document.addEventListener("DOMContentLoaded", async () => {
+//     const dados = await getUserData()
+//     if (!dados || !dados.matricula){
+//         document.getElementById("consulta-ultimas").textContent = "SEM MATRICULA";
+//     }
+//     const matricula = dados.matricula
+//     const response = await fetch(`/pacientes/atendimentos/${matricula}`)
+//     const atendimentos = await response.json()
+//     // document.getElementById("consulta-ultimas").textContent = atendimentos;
+//     const consultaElement = document.getElementById("consulta-ultimas");
+
+//     if (Array.isArray(atendimentos) && atendimentos.length > 0) {
+//         //estrutura de exibição
+//         consultaElement.innerHTML = `
+//             <ul>
+//                 ${atendimentos.map(atendimento => `
+//                     <li>
+//                         <strong>Data:</strong> ${atendimento.horario || 'Não informada'}<br>
+//                         <strong>Profissional:</strong> ${atendimento.profissional || 'Não informado'}<br>
+//                         <strong>Status:</strong> ${atendimento.status || 'Sem descrição'}<br>
+//                     </li>
+//                 `).join('')}
+//             </ul>
+//         `;
+//     } else {
+//         consultaElement.textContent = "Nenhum atendimento encontrado.";
+//     }
+
+// })
